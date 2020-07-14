dc87a9e54326e09b9ce653e3adb025cf2911d3d4
Komol Chandra Devnath <kiri2kaofficial@gmail.com>
2020-07-14 21:42:02 +0600

    Update&show

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,8 @@ Route::prefix('admin')->group(function(){
          //District
         Route::resource('/district', 'DistrictController');
         Route::post('/district/store', 'DistrictController@store');
+        Route::post('/district/update', 'DistrictController@update');
+        Route::get('/district/show/{id}', 'DistrictController@show');
+        
     });
 });
