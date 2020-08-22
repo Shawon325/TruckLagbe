098757045fb2dbc38028c15818a8aa6000d27279
Bardock325 <mynameisshawon325@gmail.com>
2020-08-22 11:36:14 +0600

    view_postC show

--- resources/views/Frontend/Layouts/frontend_css.blade.php
+++ resources/views/Frontend/Layouts/frontend_css.blade.php
@@ -1,21 +1,23 @@
 <!-- Favicons -->
-<link href="frontend_assets/assets/img/favicon.png" rel="icon">
-<link href="frontend_assets/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="{{asset('frontend_assets/assets/img/favicon.png')}}" rel="icon">
+<link href="{{asset('frontend_assets/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900"
+    rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="frontend_assets/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="frontend_assets/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="frontend_assets/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-<link href="frontend_assets/assets/vendor/venobox/venobox.css" rel="stylesheet">
-<link href="frontend_assets/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-<link href="frontend_assets/assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+<link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/vendor/aos/aos.css')}}" rel="stylesheet">
 
 <!-- Template Main CSS File -->
-<link href="frontend_assets/assets/css/style.css" rel="stylesheet">
+<link href="{{asset('frontend_assets/assets/css/style.css')}}" rel="stylesheet">
 
 <!-- =======================================================
 * Template Name: Mamba - v2.4.0
--- resources/views/Frontend/Section/post.blade.php
+++ resources/views/Frontend/Section/post.blade.php
@@ -11,13 +11,14 @@
             <div class="row no-gutters" id="post">
 
                 @foreach($post as $key => $value)
-                <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up">
-                    <span>{{$key+1}}</span>
-                    <h4>{{$value->assign_date}}</h4>
-                    <h5>{{$value->post_pick_up_time}}</h5>
-                    <p>{{$value->description}}</p>
-                    <button class="btn btn-dark">More</button>
-                </div>
+                    <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up">
+                        <span>{{$key+1}}</span>
+                        <h4>{{$value->assign_date}}</h4>
+                        <h5>{{$value->post_pick_up_time}}</h5>
+                        <p>{{$value->description}}</p>
+                        <button class="btn btn-dark"><a href="{{url('view_post' , $value->post_id)}}">More</a>
+                        </button>
+                    </div>
                 @endforeach
 
             </div>
