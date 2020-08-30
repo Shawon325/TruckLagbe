44cee2062cb64468e4ec0ec49069db9d673e15f7
Bardock325 <mynameisshawon325@gmail.com>
2020-08-31 02:10:19 +0600

    error_page made

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<img style="display: block;margin-left: auto;margin-right: auto;width: 50%;"
+     src="{{asset('backend_assets/images/status/401.webp')}}">
+</body>
+</html>
